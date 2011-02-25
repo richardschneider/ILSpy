8b141761023fb8c265139eed3ba9e5bfcdb53f76
Richard Schneider <makaretu@gmail.com>
2011-02-25 17:07:29 +1300

    more unit tests

---
+++ ICSharpCode.Decompiler.UnitTests/Issue26.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ICSharpCode.Decompiler
+{
+    /// <summary>
+    ///   passing null to a method/constructor that has multiple signatures 
+    /// </summary>
+    /// <remarks>
+    ///   When the method has multiple signatures; such as foo(MyObject o)  and foo(string s), then code calling for with a null argument must have a cast.  
+    ///   Currently ILSpy will display foo(null) and it should be foo((string) null)  or foo((MyObject) null).
+    ///   
+    ///  I found this while looking at the .ctor for System.Xml.Resolvers.XmlPreloadedResolver.
+    /// </remarks>
+    [TestClass]
+    public class Issue26 : DecompilerTest
+    {
+        public class Sample
+        {
+            public static int Switch(int i)
+            {
+                int j;
+                switch (i)
+                {
+                    case 0:
+                        {
+                            j = i + 1;
+                            break;
+                        }
+                    case 1:
+                        {
+                            j = i + 2;
+                            break;
+                        }
+                    default:
+                        {
+                            return -1;
+                        }
+                }
+                return j * 2;
+            }
+            public static int SwitchNoDefault(int i)
+            {
+                int j = 0;
+                switch (i)
+                {
+                    case 0:
+                        {
+                            j = i + 1;
+                            break;
+                        }
+                    case 1:
+                        {
+                            j = i + 2;
+                            break;
+                        }
+                }
+                return j * 2;
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ICSharpCode.Decompiler.UnitTests/Issue32.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ICSharpCode.Decompiler
+{
+    /// <summary>
+    ///   continue statements are inserted at end of block; they are not needed.
+    /// </summary>
+    [TestClass]
+    public class Issue32 : DecompilerTest
+    {
+        public class Sample
+        {
+            public void ArrayLoop(string[] list) 
+            {
+                foreach (string s in list)
+                {
+                    s.ToLower();
+                }
+            }
+
+            public void ListLoop(List<string> list)
+            {
+                foreach (string s in list)
+                {
+                    s.ToLower();
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Assert.AreEqual("private void Call1(){this.Foo((Issue27.Sample)null);}", DecompileMethod(typeof(Sample), "Call1"));
+
+        }
+    }
+}
